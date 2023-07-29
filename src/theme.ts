6074cb03aa9af2867ccea70955ce8ad510578775
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-29 22:01:22 +0400

    add initial theme

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#FFBE34'
+        },
+        secondary: {
+            main: '#091242'
+        }
+    }
+})
+
+export default theme;
