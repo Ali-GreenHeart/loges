4e7852f2c974d60c0e6744db83bd1b5c78d67c44
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-31 22:16:47 +0400

    add HeaderInfo component

---
+++ src/components/Header/HeaderInfo.tsx
@@ -0,0 +1,43 @@
+import { Box, Typography, Stack, SvgIconTypeMap } from "@mui/material"
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+
+interface Props {
+    IconElement: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+    title1: string;
+    title2: string;
+    link?: string;
+}
+
+const HeaderInfo = ({ IconElement, title1, title2, link }: Props) => {
+    return (
+        <Stack
+            flexDirection="row"
+            columnGap={1}
+            sx={{
+                '&> div > *': {
+                    color: 'white',
+                    textDecoration: 'none',
+                    fontSize: "14px"
+                }
+            }}
+        >
+            <IconElement
+                color="primary"
+                sx={{
+                    border: '1px solid',
+                    borderColor: 'rgba(17, 28, 85, 0.8)',
+                    borderRadius: '50%',
+                    p: 1,
+                    fontSize: 30,
+                    backgroundColor: 'secondary.light',
+                    boxSizing: 'content-box'
+                }}
+            />
+            <Box>
+                <Typography>{title1}</Typography>
+                <Typography component={link ? 'a' : 'p'} href={link}>{title2}</Typography>
+            </Box>
+        </Stack>
+    )
+}
+export default HeaderInfo
