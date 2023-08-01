afdf99dce79b433795b3fdb89938de280074acb0
Nicat Zeynalli <nicat21308@gmail.com>
2023-08-01 12:31:16 +0400

    Nicat_PricingCard

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,11 +1,13 @@
 import Footer from "../Footer"
 import Header from "../Header"
 
+
 const PageContainer = () => {
     return (
         <>
             <Header />
             <div>other parts...</div>
+           
             <Footer />
         </>
     )
