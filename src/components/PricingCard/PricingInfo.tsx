c5118e80881565db1bebea8f46688543a358bee5
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-08-02 21:26:15 +0400

    add gap and fix order issue

--- src/components/PricingCard/PricingInfo.tsx
+++ src/components/PricingCard/PricingInfo.tsx
@@ -1,77 +1,84 @@
 import {
-    Box,
-    Button,
-    Divider,
-    List,
-    ListItem,
-    Stack,
-    Typography,
-  } from "@mui/material";
-  
-  interface IPrice {
-      cardType : string,
-      cardPrice : number, 
-      feature1 : string
-      feature2 : string
-      feature3 : string
-      feature4 : string
-      isSpecial? : boolean
-  }
+  Box,
+  Button,
+  Divider,
+  List,
+  ListItem,
+  Stack,
+  Typography,
+} from "@mui/material";
 
-  function PricingInfo({cardType , cardPrice , feature1 , feature2 ,feature3 ,feature4 , isSpecial=false}:IPrice){
-     return (
-        <Stack 
-        rowGap={3} 
-        alignItems={"center"}
-        flexGrow={1} 
-        py={6}
-        sx={{backgroundColor: isSpecial? "secondary.main" : null , "& p,h1" : {color:isSpecial?"white" : null} }}
-            >
-          <Typography 
-          variant="h5" 
-          component="h1"
-          sx={{fontWeight:600}}>
-            {cardType}
-          </Typography>
-          <Typography 
-          variant="h2" 
-          component="h1"
-           sx={{fontWeight:700}}>
-            ${cardPrice}<span style={{fontSize:16 , fontWeight:"500"}}>/month</span>
-          </Typography>
-          <List component="nav" aria-label="mailbox folders">
-            <ListItem divider>
-              <Typography component="p" >{feature1}</Typography>
-            </ListItem>
-           <Divider sx={{boxShadow: "0 0 1px #ccc"}}/>
-            <ListItem  divider>
-        <Typography component="p">{feature2}</Typography>
-            </ListItem>
-            <Divider sx={{boxShadow: "0 0 1px #ccc"}} />
-            <ListItem divider>
-              <Typography component="p">{feature3}</Typography>
-            </ListItem>
-            <Divider sx={{boxShadow: "0 0 1px #ccc"}} />
-            <ListItem divider>
-              <Typography component="p">{feature4}</Typography>
-            </ListItem>
-            <Divider sx={{boxShadow: "0 0 1px #ccc"}}  />
-        
-          </List>
-           
-          <Button variant="contained" color={isSpecial ? "primary" : "secondary"}
-  sx={{
-    position: "relative",
-    background: isSpecial
-      ? "linear-gradient(45deg, #FFB629, #FFDA56, #FFD7A6)"
-      : "",
-  }}
->
-            Choose Plan
-            <Box bgcolor={isSpecial?"#FFFFFF":"#1F2A69"} sx={{position:"absolute", right:0 , bottom:"0px"  , borderRadius:"50%",width:20, height:10}}></Box>
-          </Button>
-        </Stack>
-     )
-  }
+interface IPrice {
+  cardType: string,
+  cardPrice: number,
+  feature1: string
+  feature2: string
+  feature3: string
+  feature4: string
+  isSpecial?: boolean
+}
 
-  export default PricingInfo
+function PricingInfo({ cardType, cardPrice, feature1, feature2, feature3, feature4, isSpecial = false }: IPrice) {
+  return (
+    <Stack
+      rowGap={3}
+      alignItems={"center"}
+      flexGrow={1}
+      mx="auto"
+      py={6}
+      sx={{
+        backgroundColor: isSpecial ? "secondary.main" : null,
+        "& p,h1": { color: isSpecial ? "white" : null },
+        minWidth: 300,
+        maxWidth: 320,
+        order: { xs: isSpecial ? '3' : 'unset', lg: 'unset' }
+      }}
+    >
+      <Typography
+        variant="h5"
+        component="h1"
+        sx={{ fontWeight: 600 }}>
+        {cardType}
+      </Typography>
+      <Typography
+        variant="h2"
+        component="h1"
+        sx={{ fontWeight: 700 }}>
+        ${cardPrice}<span style={{ fontSize: 16, fontWeight: "500" }}>/month</span>
+      </Typography>
+      <List component="nav" aria-label="mailbox folders">
+        <ListItem divider>
+          <Typography component="p" >{feature1}</Typography>
+        </ListItem>
+        <Divider sx={{ boxShadow: "0 0 1px #ccc" }} />
+        <ListItem divider>
+          <Typography component="p">{feature2}</Typography>
+        </ListItem>
+        <Divider sx={{ boxShadow: "0 0 1px #ccc" }} />
+        <ListItem divider>
+          <Typography component="p">{feature3}</Typography>
+        </ListItem>
+        <Divider sx={{ boxShadow: "0 0 1px #ccc" }} />
+        <ListItem divider>
+          <Typography component="p">{feature4}</Typography>
+        </ListItem>
+        <Divider sx={{ boxShadow: "0 0 1px #ccc" }} />
+
+      </List>
+
+      <Button variant="contained" color={isSpecial ? "primary" : "secondary"}
+        sx={{
+          position: "relative",
+          background: isSpecial
+            ? "linear-gradient(45deg, #FFB629, #FFDA56, #FFD7A6)"
+            : "",
+        }}
+      >
+        Choose Plan
+        <Box bgcolor={isSpecial ? "#FFFFFF" : "#1F2A69"} sx={{ position: "absolute", right: 0, bottom: "0px", borderRadius: "50%", width: 20, height: 10 }}></Box>
+      </Button>
+    </Stack>
+  )
+}
+
+export default PricingInfo
--- src/components/PricingCard/index.tsx
+++ src/components/PricingCard/index.tsx
@@ -1,38 +1,38 @@
-import {Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import PricingInfo from "./PricingInfo";
 
 const prices = [
-    {
-        key: 0 ,
-        cardType :"Basic",
-        cardPrice : 39, 
-        feature1 : "Single Truck",
-        feature2 : "Full Insurance",
-        feature3 : "500 Km",
-        feature4 : "Real-time rate shopping",
-       
-    },
-    {
-        key: 1 ,
-        cardType :"Standart",
-        cardPrice : 59, 
-        feature1 : "Single Truck",
-        feature2 : "Full Insurance",
-        feature3 : "500 Km",
-        feature4 : "Real-time rate shopping",
-        isSpecial : true
-        
-    },
-    {
-        key: 2 ,
-        cardType :"Premium",
-        cardPrice : 89, 
-        feature1 : "Double Truck",
-        feature2 : "Full Insurance",
-        feature3 : "Unlimitted",
-        feature4 : "Real-time rate shopping",
-        
-    }
+  {
+    key: 0,
+    cardType: "Basic",
+    cardPrice: 39,
+    feature1: "Single Truck",
+    feature2: "Full Insurance",
+    feature3: "500 Km",
+    feature4: "Real-time rate shopping",
+
+  },
+  {
+    key: 1,
+    cardType: "Standart",
+    cardPrice: 59,
+    feature1: "Single Truck",
+    feature2: "Full Insurance",
+    feature3: "500 Km",
+    feature4: "Real-time rate shopping",
+    isSpecial: true
+
+  },
+  {
+    key: 2,
+    cardType: "Premium",
+    cardPrice: 89,
+    feature1: "Double Truck",
+    feature2: "Full Insurance",
+    feature3: "Unlimitted",
+    feature4: "Real-time rate shopping",
+
+  }
 ]
 
 
@@ -41,29 +41,26 @@ function PricingCard() {
     <Container maxWidth="lg">
       <Stack alignItems={"center"}>
         <Stack flexDirection={"row"}
-        justifyContent={"center"}
-        alignItems={"center"}>
-            <Box sx={{height:23, borderLeft:"4px solid #FFBE34",}}></Box>
-          <Typography component="p" sx={{ fontSize: 10 , padding:2}}>
-          Pricing
+          justifyContent={"center"}
+          alignItems={"center"}>
+          <Box sx={{ height: 23, borderLeft: "4px solid #FFBE34", }}></Box>
+          <Typography component="p" sx={{ fontSize: 10, padding: 2 }}>
+            Pricing
           </Typography>
         </Stack>
         <Typography variant="h4" component="h1" sx={{ fontWeight: 700 }}>
           Our Best Pricing
         </Typography>
       </Stack>
-      
-      <Stack flexDirection="row" my={8} flexWrap={"wrap"} rowGap={10} >
-        { 
-         
-            prices.map((price)=>{
-                return (
-                    <PricingInfo {...price}/>
-                    )
-                })
-            }
+
+      <Stack flexDirection="row" my={8} flexWrap={"wrap"} gap={5}
+      >
+        {
+
+          prices.map((price) => <PricingInfo {...price} />)
+        }
       </Stack>
-        
+
     </Container>
   );
 }
