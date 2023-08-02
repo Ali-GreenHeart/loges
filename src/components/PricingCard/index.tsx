e8132c7ad695670eda8369cd7f38bb48c0d07242
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-08-02 21:35:13 +0400

    fix type issue and flex issue

--- src/components/PricingCard/index.tsx
+++ src/components/PricingCard/index.tsx
@@ -3,7 +3,23 @@ import React from "react";
 import { useState, useEffect } from "react";
 import "./module.css"
 import pattern from "../PricingCard"
-const Package: any = [
+
+interface PricingCardType {
+  id: number;
+  title: string;
+  price: {
+    monthlyPrice: string;
+    date: string;
+  },
+  specifications: {
+    truck: string;
+    insurance: string;
+    distance: string;
+    ratings: string;
+  }
+}
+
+const Package: PricingCardType[] = [
   {
     id: 0,
     title: "Basic",
@@ -51,41 +67,40 @@ const Package: any = [
 const PricingCard = () => {
   return (
     <>
-      {Package.map((item: any) => (
-        <Stack className="parent" direction="row" spacing={2}
-          key={item.id}
-        >
-          <Box className="main" sx={{
-            
+      <Stack
+        justifyContent="center"
+        className="parent" direction="row" spacing={2}>
+        {Package.map((item: any) => (
+          <Box className="main"
+            key={item.id}
+            sx={{
               backgroundColor: "#F4F4F4",
               width: 250,
               display: "flex",
-              gap:"20px",
+              gap: "20px",
               flexDirection: "column",
-              transition:"0.5s all",
-              
-
-              "&:hover":{
-                backgroundColor:"#091242",
-                color:"white"
+              transition: "0.5s all",
+              "&:hover": {
+                backgroundColor: "#091242",
+                color: "white"
               },
-              "&:hover p":{
+              "&:hover p": {
                 // backgroundColor:"#091242",
                 // transform:'translateY(-8%)',
-                color:"white !important"
+                color: "white !important"
               },
-              "&:hover button":{
+              "&:hover button": {
                 // backgroundColor:"#091242",
                 // transform:'translateY(-8%)',
-                backgroundColor:"#FFB629",
-                color:"#091242",
+                backgroundColor: "#FFB629",
+                color: "#091242",
                 // backgroundImage:,
               }
-          }}
+            }}
 
           >
             <h3>{item.title}</h3>
-            <aside style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+            <aside style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
               <h1 className="price">{item.price.monthlyPrice}</h1>
               <p>{item.price.date}</p>
             </aside>
@@ -93,26 +108,26 @@ const PricingCard = () => {
               <p className="specification">{item.specifications.truck}</p>
               <p className="specification">{item.specifications.insurance}</p>
               <p className="specification">{item.specifications.distance}</p>
-              <p className="specification" style={{borderBottom:"1px solid #d6d6d680"}}>{item.specifications.ratings}</p>
+              <p className="specification" style={{ borderBottom: "1px solid #d6d6d680" }}>{item.specifications.ratings}</p>
             </div>
             <Button className="chooseButton" sx={{
-                backgroundColor:"#091242",
-                color:"#FFB629",
-                transition:"0.5s all",
-                fontWeight:600,
-        
-               
-              "&:hover":{
+              backgroundColor: "#091242",
+              color: "#FFB629",
+              transition: "0.5s all",
+              fontWeight: 600,
+
+
+              "&:hover": {
                 // backgroundPosition:"inherit"
-                backgroundColor:"#091242"
+                backgroundColor: "#091242"
               }
             }}>
               Choose Plan
-           
+
             </Button>
           </Box>
-        </Stack>
-      ))}
+        ))}
+      </Stack>
     </>
   );
 };
