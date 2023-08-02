593775cae9aa40c5eac86fdaa6f3a9d0e2b3136d
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-08-02 22:10:08 +0400

    customize textfield component

--- src/theme.ts
+++ src/theme.ts
@@ -9,6 +9,26 @@ const theme = createTheme({
             main: '#091242',
             light: 'rgba(39, 50, 112,0.6)'
         }
+    },
+    components: {
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    '& input': {
+                        color: 'rgba(255,255,255,0.5)'
+                    },
+                    '& label': {
+                        color: 'rgba(255,255,255,0.5)'
+                    },
+                    '& fieldset': {
+                        borderColor: 'rgba(255,255,255,0.5)',
+                    },
+                    '& fieldset:hover': {
+                        borderColor: 'rgba(255,255,255,0.5)',
+                    }
+                }
+            }
+        }
     }
 })
 
