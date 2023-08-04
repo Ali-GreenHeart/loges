a83628cd5287a972b2698a5b86b9288eac59ce13
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-08-04 21:21:20 +0400

    add serviceCard by Aynur

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,15 +1,14 @@
 import Footer from "../Footer"
 import Header from "../Header"
-import ServiceCard from "../ServiceCardd/ServiceCard"
 
-    
+
 
 
 const PageContainer = () => {
     return (
         <>
             <Header />
-           <ServiceCard/>
+            <main>test</main>
 
             <Footer />
         </>
--- src/components/ServiceCardd/ServiceCard.tsx
+++ src/components/ServiceCardd/ServiceCard.tsx
@@ -1,33 +1,34 @@
 
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
-import iconn from "../ServiceCardd/iconn.jpg"
 
-export default function ServiceCard() {
+interface IProps {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+export default function ServiceCard({ icon, title, description }: IProps) {
   return (
     <Card sx={{ maxWidth: 320, p: 2, width: 270, height: 193 }}>
-
       <Typography>
-        <img style={{ width: 35 }} src={iconn} alt="hip" />
+        <img style={{ width: 35 }} src={icon} alt="hip" />
       </Typography>
 
-      <Button size="small" color="primary"
-        sx={{
+      <Typography
+        sx={(theme) => ({
           ":hover": {
-            color: "gray",
-            backgroundColor: "white"
-
+            color: theme.palette.primary.main,
+            cursor: 'default'
           }
-        }}>
-        Sea Transport Services
-      </Button>
+        })}
+      >
+        {title}
+      </Typography>
 
       <Typography variant="body2" color="text.secondary">
-        Following the quality of our service thus having gained trust of our many clients.
+        {description}
       </Typography>
-
-
-    </Card>
+    </Card >
   );
 }
