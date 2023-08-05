3d1772191b5cf6a811a88c7f1ceceee744db59ce
Nicat Zeynalli <nicat21308@gmail.com>
2023-08-05 19:17:23 +0400

    Page Project

---
+++ src/pages/Project/index.tsx
@@ -0,0 +1,18 @@
+
+import { Box } from '@mui/material'
+import PageContainer from '../../components/PageContainer'
+import WhyUs from '../../components/WhyUs'
+import FooterInfo from '../../components/WhyUs/footerInfo'
+
+const Project = () => {
+  return (
+    <PageContainer >
+        <Box>
+   <WhyUs/>
+   <FooterInfo/>
+        </Box>
+    </PageContainer>
+  )
+}
+
+export default Project
