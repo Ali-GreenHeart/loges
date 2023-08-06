4267cc7ce5af7d0d4438873617bbc6235a396592
Orkhan647 <okuw.k@mail.ru>
2023-08-06 22:40:54 +0400

    Update HowWeWork Section

--- src/components/HowWeWork/index.tsx
+++ src/components/HowWeWork/index.tsx
@@ -12,45 +12,56 @@ const HowWeWork = () => {
   return (
     <>
       <Header />
-      <Container sx={{ maxWidth: "800px", padding: "10px 10px 50px 10px " }}>
-        <SectionHeader heading={"Our Goodness"} subheading={"How We Works"} />
-        <Box display="flex" flexDirection="row" gap="50px">
-          <Box sx={mainImgContainerStyle}>
-            <CardMedia component="img" image={MainImg} alt="worker" />
-            <CardMedia
-              component="img"
-              image={SecondImg}
-              alt="state"
-              sx={secondImgStyle}
-            />
-          </Box>
-          <Box sx={{ display: "flex", flexDirection: "column", gap:"5px" }}>
-            <Box display="flex" alignItems="center">
-              <Typography>
-                <img style={{ width: 35 }} src={Icon1} alt="Box Icon" />
-              </Typography>
-              <Typography variant="h5">Warehousing Services</Typography>
+      <Box sx={{ backgroundColor: "#F4F4F4" }}>
+        <Container sx={{ maxWidth: "800px", padding: "10px 10px 50px 10px" }}>
+          <SectionHeader heading={"Our Goodness"} subheading={"How We Works"} />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "50px",
+            }}
+          >
+            
+            <Box sx={mainImgContainerStyle}>
+              <CardMedia component="img" image={MainImg} alt="worker" />
+              <CardMedia component="img" image={SecondImg} alt="state" sx={secondImgStyle} />
             </Box>
-            <Typography variant="body1">Leverage agile frameworks to provide a robust synopsis for strategy foster collaborative thinking</Typography>
 
-            <Box display="flex" alignItems="center">
-              <Typography>
-                <img style={{ width: 35 }} src={Icon2} alt="Sheild Icon" />
+            <Box sx={{ display: "flex", flexDirection: "column", gap: "20px", marginLeft: "40px" }}>
+              <Box display="flex" alignItems="center">
+                <Typography>
+                  <img style={{ width: 35 }} src={Icon1} alt="Box Icon" />
+                </Typography>
+                <Typography variant="h5">Warehousing Services</Typography>
+              </Box>
+              <Typography variant="body1">
+                Leverage agile frameworks to provide a robust synopsis for strategy foster collaborative thinking
               </Typography>
-              <Typography variant="h6">Safety & Quality</Typography>
-            </Box>
-            <Typography variant="body1">Leverage agile frameworks to provide a robust synopsis for strategy foster collaborative thinking</Typography>
 
-            <Box display="flex" alignItems="center">
-              <Typography>
-                <img style={{ width: 35 }} src={Icon3} alt="Environment Icon" />
+              <Box display="flex" alignItems="center">
+                <Typography>
+                  <img style={{ width: 35 }} src={Icon2} alt="Sheild Icon" />
+                </Typography>
+                <Typography variant="h6">Safety & Quality</Typography>
+              </Box>
+              <Typography variant="body1">
+                Leverage agile frameworks to provide a robust synopsis for strategy foster collaborative thinking
               </Typography>
-              <Typography variant="h6">Care for Environment</Typography>
+
+              <Box display="flex" alignItems="center">
+                <Typography>
+                  <img style={{ width: 35 }} src={Icon3} alt="Environment Icon" />
+                </Typography>
+                <Typography variant="h6">Care for Environment</Typography>
+              </Box>
+              <Typography variant="body1">
+                Leverage agile frameworks to provide a robust synopsis for strategy foster collaborative thinking
+              </Typography>
             </Box>
-            <Typography variant="body1">Icon 3 description here</Typography>
           </Box>
-        </Box>
-      </Container>
+        </Container>
+      </Box>
       <Footer />
     </>
   );
@@ -58,7 +69,7 @@ const HowWeWork = () => {
 
 const mainImgContainerStyle = {
   position: "relative",
-  width: "550px",
+  width: "650px",
 };
 
 const secondImgStyle = {
