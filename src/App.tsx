8d00f97688b748c9eeb781a7d2aab23bc4bd4cab
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-08-09 21:26:33 +0400

    remove redundant codes on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import PasswordProtection from "./components/PasswordProtection"
-import PlaneSection from "./components/Plane"
 import WebRouting from "./routing"
 
 function App() {
   return (
     <>
       <WebRouting />
-      <PlaneSection/>
-      <PasswordProtection title="Enter Password"/>
-
     </>
   )
 }
