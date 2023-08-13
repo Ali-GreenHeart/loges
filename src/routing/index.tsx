baa8a82750cff21d219888d026176c47f2fe7fd8
Orkhan647 <okuw.k@mail.ru>
2023-08-13 21:40:49 +0400

    add cards and texts

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -3,6 +3,7 @@ import { Suspense, lazy } from 'react';
 import Loading from "../components/Loading";
 import PasswordProtection from "../components/PasswordProtection";
 import Project from "../pages/Project";
+import AirCargo from "../pages/AirCargo";
 
 const HomePage = lazy(() => import("../pages/Home"));
 
@@ -14,6 +15,7 @@ const WebRouting = () => {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/password-protection" element={<PasswordProtection />} />
                 <Route path="/project" element={<Project/>}/>
+                <Route path="/air-cargo" element={<AirCargo/>}/>
                 
             </Routes>
         </Suspense>
