1f92eee2cf79cb2e3a43b1c62dd1edc9f31a506a
Nicat Zeynalli <nicat21308@gmail.com>
2023-08-15 11:57:16 +0400

    NavBar and MenuBar added!

--- src/components/Banner/NavLink.tsx
+++ src/components/Banner/NavLink.tsx
@@ -1,39 +1,108 @@
-import { Divider } from "@mui/material";
-import React from "react"
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import { Divider, Stack } from "@mui/material";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 interface IProps {
-    id: number;
-    to: string;
-    page: string;
-    handleCloseNavMenu: any;
-    pagesLength: number;
-    pages?: any;
+  id: number;
+  to: string;
+  page: string;
+  handleCloseNavMenu: any;
+  pagesLength: number;
+  pages?: any;
 }
 
-const NavLink = ({ id, to, pagesLength, page, handleCloseNavMenu, pages }: IProps) => {
-    const openDropDown = () => {
-        alert('sdjbfh')
-    }
-    return (
-        <React.Fragment>
-            <Link onMouseEnter={pages && openDropDown} to={to} onClick={handleCloseNavMenu}>
-                {page}
-                {
-                    pages && ">"
-                }
-            </Link>
-            {id !== pagesLength - 1 && (
-                <Divider
-                    sx={{
-                        width: "1px",
-                        height: "18px",
-                        alignSelf: "center",
-                        backgroundColor: "rgba(255,255,255,0.2)",
-                    }}
-                />
-            )}
-        </React.Fragment>
-    )
-}
-export default NavLink
+const NavLink = ({
+  id,
+  to,
+  pagesLength,
+  page,
+  handleCloseNavMenu,
+  pages,
+}: IProps) => {
+  const [pg, setPg] = useState(false);
+
+  const openDropDown = () => {
+    setPg(true);
+  };
+
+  const closeDropDown = () => {
+    setPg(false);
+  };
+
+  return (
+    <React.Fragment>
+      {pages ? (
+        <Stack
+          onMouseEnter={openDropDown}
+          onMouseLeave={closeDropDown}
+          sx={{ textDecoration: "none", cursor: "pointer" }}
+        >
+          <Link
+            to={pages ? "" : to}
+            onClick={handleCloseNavMenu}
+            style={{ textDecoration: "none" }}
+          >
+            <Stack flexDirection="row" sx={{ position: "relative" }}>
+              {page}
+              {!pg ? <ExpandMore /> : <ExpandLess />}
+            </Stack>
+          </Link>
+          {pg && (
+            <Stack
+              px={4}
+              height={200}
+              overflow="auto"
+              py={2}
+              gap={2}
+              sx={{
+                position: "absolute",
+                top: 60,
+                backgroundColor: "secondary.main",
+                "&::-webkit-scrollbar": {
+                  width: "8px",
+                },
+                "&::-webkit-scrollbar-thumb": {
+                  backgroundColor: "rgba(0, 0, 0, 0.2)",
+                },
+                "&::-webkit-scrollbar-track": {
+                  backgroundColor: "transparent",
+                },
+              }}
+            >
+              {pages.map((page: any) => (
+                <Stack key={page.id} sx={{ "&>a": { p: 0 } }}>
+                  <Link to={page.to} style={{ textDecoration: "none" }}>
+                    {page.page}
+                  </Link>
+                </Stack>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      ) : (
+        <Link
+          to={to}
+          onClick={handleCloseNavMenu}
+          style={{ textDecoration: "none" }}
+        >
+          <Stack flexDirection="row" sx={{ position: "relative" }}>
+            {page}
+          </Stack>
+        </Link>
+      )}
+
+      {id !== pagesLength - 1 && (
+        <Divider
+          sx={{
+            width: "1px",
+            height: "18px",
+            alignSelf: "center",
+            backgroundColor: "rgba(255,255,255,0.2)",
+          }}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+export default NavLink;
