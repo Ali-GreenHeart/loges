2d3586dc923fd08b5f88bd7f588b51365d0fb368
Orkhan647 <okuw.k@mail.ru>
2023-08-16 00:04:31 +0400

    Added Team Page and removed outdated images

---
+++ src/pages/Team-Page/index.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import PageContainer from '../../components/PageContainer'
+import { Typography } from '@mui/material'
+
+const TeamPage = () => {
+  return (
+   <PageContainer>
+    <Typography>salam</Typography>
+   </PageContainer>
+  )
+}
+
+export default TeamPage
