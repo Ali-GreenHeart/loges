6870319e982864ee127d3c80d437e231c97dea18
Orkhan647 <okuw.k@mail.ru>
2023-08-16 00:10:37 +0400

    Updated routing configuration in src/routing/index.tsx

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,25 +1,25 @@
 import { Route, Routes } from "react-router-dom";
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy } from "react";
 import Loading from "../components/Loading";
 import PasswordProtection from "../components/PasswordProtection";
 import Project from "../pages/Project";
-import AirCargo from "../pages/AirCargo";
+
+import TeamPage from "../pages/Team-Page";
 
 const HomePage = lazy(() => import("../pages/Home"));
 
 
 const WebRouting = () => {
-    return (
-        <Suspense fallback={<Loading />}>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/password-protection" element={<PasswordProtection />} />
-                <Route path="/project" element={<Project/>}/>
-                <Route path="/air-cargo" element={<AirCargo/>}/>
-                
-            </Routes>
-        </Suspense>
-    );
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/password-protection" element={<PasswordProtection />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/Team" element={<TeamPage />} />
+      </Routes>
+    </Suspense>
+  );
 };
 
 export default WebRouting;
