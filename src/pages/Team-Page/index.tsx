3b426899640a7cec035e9fda8130cb43f73c9ff1
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-08-16 21:47:59 +0400

    add team page by Orkhan

--- src/pages/Team-Page/index.tsx
+++ src/pages/Team-Page/index.tsx
@@ -13,7 +13,7 @@ import SectionHeader from "../../components/SectionHeader";
 import Members from "./Members";
 import userIcon from "../../assets/Team-Page/UserIcon.png";
 import HourglassIcon from "../../assets/Team-Page/HourglassIcon .png";
-import Sponsors from "./Sponsors";
+import Sponsors from "../../components/Sponsors";
 
 function Team() {
   return (
@@ -22,182 +22,178 @@ function Team() {
       bannerTitle="Our Team"
       bannerSubtitle="Team"
     >
-      <Container>
-        <SectionHeader
-          heading={"The Transporters"}
-          subheading={"Meet Expert Team"}
-        />
-        <Stack
-          flexDirection="row"
-          justifyContent="center"
-          flexWrap="wrap"
-          gap={5}
-        >
-          <Members
-            imgSrc={Person1}
-            name="Jessca Arrow"
-            job="Designer"
-            facebookLink="guya"
-            linkedInLink="guya2"
-            twitterLink="sfdg"
+      <Box bgcolor="#F4F4F4" sx={{ my: -10, py: 10, mb: 10 }}>
+        <Container>
+          <SectionHeader
+            heading={"The Transporters"}
+            subheading={"Meet Expert Team"}
           />
-          <Members
-            imgSrc={Person2}
-            name="Kathleen Smith"
-            job="Designer"
-            facebookLink="guya"
-            linkedInLink="guya2"
-            instagramLink="sfdg"
-          />
-          <Members
-            imgSrc={Person3}
-            name="Rebecca Tylor"
-            job="Designer"
-            facebookLink="guya"
-            linkedInLink="guya2"
-          />
-          <Members
-            imgSrc={Person4}
-            name="Rebecca Tylor"
-            job="Designer"
-            facebookLink="guya"
-            linkedInLink="guya2"
-          />
-          <Members
-            imgSrc={Person5}
-            name="Rebecca Tylor"
-            job="Designer"
-            facebookLink="guya"
-            linkedInLink="guya2"
-          />
-          <Members
-            imgSrc={Person6}
-            name="Rebecca Tylor"
-            job="Designer"
-            facebookLink="guya"
-            linkedInLink="guya2"
-          />
-        </Stack>
-
-        <Container
-          maxWidth="xl"
-          sx={{
-            padding: "10px 10px 50px 10px",
-          }}
-        >
-          <Grid
-            container
-            spacing={5}
-            justifyContent={{ md: "space-evenly", lg: "center" }}
-            alignItems={{ xs: "center", lg: "stretch" }}
+          <Stack
+            flexDirection="row"
+            justifyContent="center"
+            flexWrap="wrap"
+            gap={5}
           >
-            <Grid xs={12} md={5} lg={5} item>
-              <Box sx={mainImgContainerStyle}>
-                <img src={MainImg} style={{ width: "100%" }} alt="scania TIR" />
-                <img
-                  className="about_us_texture_img"
-                  src={SecondImg}
-                  alt="state"
-                  style={{
-                    position: "absolute",
-                    bottom: -40,
-                    right: -60,
-                    width: "45%",
-                    height: "60%",
-                    zIndex: 3,
-                    objectFit: "cover",
-                    objectPosition: "top",
-                  }}
-                />
-              </Box>
-            </Grid>
-
-            <Grid xs={12} md={5} lg={5} item>
-              <Container>
-                <Stack spacing={2}>
-                  <Typography
-                    component="p"
-                    bgcolor="rgba(9, 18, 66, 0.1)"
-                    sx={(t) => ({
-                      borderLeft: `4px solid ${t.palette.primary.main}`,
-                      fontSize: 14,
-                      p: "3px",
-                      px: 2,
-                      width: "fit-content",
-                    })}
-                  >
-                    About
-                  </Typography>
-                  <Typography
-                    variant="h4"
-                    component="h4"
-                    sx={{ fontWeight: 700, textTransform: "capitalize" }}
-                  >
-                    Why Choose Us
-                  </Typography>
-                  <Typography color={"#666C89"}>
-                    Leverage agile frameworks to provide a robust synopsis for
-                    strategy foster collaborative thinking to further the
-                    overall value proposition.
-                  </Typography>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                      <Stack
-                        direction="row"
-                        alignItems="center"
-                        spacing={2}
-                        sx={{ marginBottom: { xs: 2, sm: 0 } }}
-                      >
-                        <img src={userIcon} alt="UserIcon" />
-                        <Typography
-                          variant="h5"
-                          component="h5"
-                          sx={{ fontWeight: 700, textTransform: "capitalize" }}
-                        >
-                          Our Vision
-                        </Typography>
-                      </Stack>
-                      <Typography color="#666C89">
-                        Leverage agile frameworks to provide a robust synopsis for strategy
-                        foster.
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <Stack
-                        direction="row"
-                        alignItems="center"
-                        spacing={2}
-                        sx={{ marginBottom: { xs: 2, sm: 0 } }}
-                      >
-                        <img src={HourglassIcon} alt="HourglassIcon" />
-                        <Typography
-                          variant="h5"
-                          component="h5"
-                          sx={{ fontWeight: 700, textTransform: "capitalize" }}
-                        >
-                          Estimate Shipping
-                        </Typography>
-                      </Stack>
-                      <Typography color="#666C89">
-                        Leverage agile frameworks to provide a robust synopsis for strategy
-                        foster.
-                      </Typography>
-                    </Grid>
-                  </Grid>
-
-                </Stack>
-                <Stack
-                  direction="row"
-                  justifyContent="flex-start"
-                  sx={{ marginTop: "20px" }}
-                ></Stack>
-              </Container>
-            </Grid>
-          </Grid>
+            <Members
+              imgSrc={Person1}
+              name="Jessca Arrow"
+              job="Designer"
+              facebookLink="guya"
+              linkedInLink="guya2"
+              twitterLink="sfdg"
+            />
+            <Members
+              imgSrc={Person2}
+              name="Kathleen Smith"
+              job="Designer"
+              facebookLink="guya"
+              linkedInLink="guya2"
+              instagramLink="sfdg"
+            />
+            <Members
+              imgSrc={Person3}
+              name="Rebecca Tylor"
+              job="Designer"
+              facebookLink="guya"
+              linkedInLink="guya2"
+            />
+            <Members
+              imgSrc={Person4}
+              name="Rebecca Tylor"
+              job="Designer"
+              facebookLink="guya"
+              linkedInLink="guya2"
+            />
+            <Members
+              imgSrc={Person5}
+              name="Rebecca Tylor"
+              job="Designer"
+              facebookLink="guya"
+              linkedInLink="guya2"
+            />
+            <Members
+              imgSrc={Person6}
+              name="Rebecca Tylor"
+              job="Designer"
+              facebookLink="guya"
+              linkedInLink="guya2"
+            />
+          </Stack>
         </Container>
-        <Sponsors />
+      </Box>
+      <Container
+        maxWidth="xl"
+        sx={{
+          padding: "10px 10px 50px 10px",
+          mt: 10
+        }}
+      >
+        <Grid
+          container
+          spacing={5}
+          justifyContent={{ md: "space-evenly", lg: "center" }}
+          alignItems={{ xs: "center", lg: "stretch" }}
+        >
+          <Grid xs={12} md={5} lg={5} item>
+            <Box sx={mainImgContainerStyle}>
+              <img src={MainImg} style={{ width: "100%" }} alt="scania TIR" />
+              <img
+                className="about_us_texture_img"
+                src={SecondImg}
+                alt="state"
+                style={{
+                  position: "absolute",
+                  bottom: -40,
+                  right: -60,
+                  width: "45%",
+                  height: "60%",
+                  zIndex: 3,
+                  objectFit: "cover",
+                  objectPosition: "top",
+                }}
+              />
+            </Box>
+          </Grid>
+
+          <Grid xs={12} md={5} lg={5} item>
+            <Container>
+              <Stack spacing={2}>
+                <Typography
+                  component="p"
+                  bgcolor="rgba(9, 18, 66, 0.1)"
+                  sx={(t) => ({
+                    borderLeft: `4px solid ${t.palette.primary.main}`,
+                    fontSize: 14,
+                    p: "3px",
+                    px: 2,
+                    width: "fit-content",
+                  })}
+                >
+                  About
+                </Typography>
+                <Typography
+                  variant="h4"
+                  component="h4"
+                  sx={{ fontWeight: 700, textTransform: "capitalize" }}
+                >
+                  Why Choose Us
+                </Typography>
+                <Typography color={"#666C89"}>
+                  Leverage agile frameworks to provide a robust synopsis for
+                  strategy foster collaborative thinking to further the
+                  overall value proposition.
+                </Typography>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <Stack
+                      direction="row"
+                      alignItems="center"
+                      spacing={2}
+                      sx={{ marginBottom: { xs: 2, sm: 0 } }}
+                    >
+                      <img src={userIcon} alt="UserIcon" />
+                      <Typography
+                        variant="h5"
+                        component="h5"
+                        sx={{ fontWeight: 700, textTransform: "capitalize" }}
+                      >
+                        Our Vision
+                      </Typography>
+                    </Stack>
+                    <Typography color="#666C89">
+                      Leverage agile frameworks to provide a robust synopsis for strategy
+                      foster.
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <Stack
+                      direction="row"
+                      alignItems="center"
+                      spacing={2}
+                      sx={{ marginBottom: { xs: 2, sm: 0 } }}
+                    >
+                      <img src={HourglassIcon} alt="HourglassIcon" />
+                      <Typography
+                        variant="h5"
+                        component="h5"
+                        sx={{ fontWeight: 700, textTransform: "capitalize" }}
+                      >
+                        Estimate Shipping
+                      </Typography>
+                    </Stack>
+                    <Typography color="#666C89">
+                      Leverage agile frameworks to provide a robust synopsis for strategy
+                      foster.
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Stack>
+            </Container>
+          </Grid>
+        </Grid>
       </Container>
-    </PageContainer>
+      <Sponsors />
+    </PageContainer >
   );
 }
 const mainImgContainerStyle = {
