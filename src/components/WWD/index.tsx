d4f8276ef1de01101dee2ba7a41515ef352a5c7b
rahib13 <rahibismayilov123@gmail.com>
2023-08-22 19:14:42 +0400

    WWD component by rahib but what is this bro?

--- src/components/WWD/index.tsx
+++ src/components/WWD/index.tsx
@@ -1,47 +1,49 @@
 import { Box, Container, Stack } from "@mui/material";
 import React from "react";
 import SectionHeader from "../SectionHeader";
-import ship from "../../assets/wwd icons/Ship.png";
-import plane from "../../assets/wwd icons/plane.svg";
-import truck from "../../assets/wwd icons/Truck.png";
-import tower from "../../assets/wwd icons/Tower.png";
+import ship from "../../assets/wwdicons/Ship.png";
+import plane from "../../assets/wwdicons/plane.svg";
+import truck from "../../assets/wwdicons/Truck.png";
+import tower from "../../assets/wwdicons/Tower.png";
 import PageContainer from "../PageContainer";
 
 interface IProps {
-  id: {
-    icon: string;
-    title: string;
-    subtitle: string;
-  };
+  id: string;
+  icon: string;
+  title: string;
+  subtitle: string;
 }
 
-
-
-// const servisler:IProps{
-//     0{
-//         icon:{ship},
-//         title:"Sea Transport Services",
-//         subtitle:"Following the quality of our service thus having gained trust of our many clients."
-//     },
-//     1{
-//         icon:{tower},
-//         title:"Warehousing Services",
-//         subtitle:"Following the quality of our service thus having gained trust of our many clients."
-//     },
-//     2{
-//         icon:{plane},
-//         title:"Air Fright Services",
-//         subtitle:"Following the quality of our service thus having gained trust of our many clients."
-//     },
-//     3{
-//         icon:{truck},
-//         title:"Local Shipping Services",
-//         subtitle:"Following the quality of our service thus having gained trust of our many clients."
-//     },
-// }
-
-
-
+const servisler = [
+  {
+    id: 0,
+    icon: ship,
+    title: "Sea Transport Services",
+    subtitle:
+      "Following the quality of our service thus having gained trust of our many clients.",
+  },
+  {
+    id: 1,
+    icon: tower,
+    title: "Warehousing Services",
+    subtitle:
+      "Following the quality of our service thus having gained trust of our many clients.",
+  },
+  {
+    id: 2,
+    icon: plane,
+    title: "Air Fright Services",
+    subtitle:
+      "Following the quality of our service thus having gained trust of our many clients.",
+  },
+  {
+    id: 3,
+    icon: truck,
+    title: "Local Shipping Services",
+    subtitle:
+      "Following the quality of our service thus having gained trust of our many clients.",
+  },
+];
 
 function WhatWeDo() {
   return (
@@ -50,130 +52,76 @@ function WhatWeDo() {
         <Stack
           sx={{
             flexDirection: { sx: "column", md: "row" },
-            alignItems:{xs:"center", md:"inherit"}
-        
+            alignItems: { xs: "center", md: "inherit" },
           }}
         >
           <Box
             sx={{
-              width: { xs: "100%", md: "100%" },
+              width: { xs: "100%", md: "50%" },
               display: "flex",
               gap: "20px",
             }}
           >
-            <SectionHeader
+            <SectionHeader 
               heading="What We Do"
               subheading="Safe & Reliable Cargo Solutions"
             />
           </Box>
-          <Stack
+          <Box
             sx={{
-              flexDirection: { sx: "column", md: "row" },
-              gap: "50px",
-              flexWrap: "wrap",
+              width: "100%",
+              display: "flex",
+              // backgroundColor: "red",
             }}
           >
-            <Box
+            <Stack
               sx={{
-                width: { xs: "100%", md: "45%" },
                 display: "flex",
-                gap: "20px",
+                width: "100%",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                gap: "40px",
+                flexWrap: "wrap",
               }}
             >
-              <Box>
-                <img src={ship} alt="Ship" />
-              </Box>
-              <Box
-                sx={{
-                  borderLeft: "1px solid #D8D8D8",
-                  paddingLeft: "10px",                   
-                }}
-              >
-                <h3 style={{color:"#1C1F35"}}>Sea Transport Services</h3>
-                <p style={{color:"#666C89"}}>
-                  Following the quality of our service thus having gained trust
-                  of our many clients.
-                </p>
-              </Box>
-            </Box>
-
-            {/* Bunnan sonrasi muveqqetidir!!!! */}
-
-            <Box
-              sx={{
-                width: { xs: "100%", md: "45%" },
-                display: "flex",
-                gap: "20px",
-              }}
-            >
-              <Box>
-                <img src={ship} alt="Ship" />
-              </Box>
-              <Box
-                sx={{
-                  borderLeft: "1px solid #D8D8D8",
-                  paddingLeft: "10px",
-                   
-                }}
-              >
-                <h3 style={{color:"#1C1F35"}}>Sea Transport Services</h3>
-                <p style={{color:"#666C89"}}>
-                  Following the quality of our service thus having gained trust
-                  of our many clients.
-                </p>
-              </Box>
-            </Box>
-            <Box
-              sx={{
-                width: { xs: "100%", md: "45%" },
-                display: "flex",
-                gap: "20px",
-              }}
-            >
-              <Box>
-                <img src={ship} alt="Ship" />
-              </Box>
-              <Box
-                sx={{
-                  borderLeft: "1px solid #D8D8D8",
-                  paddingLeft: "10px",
-                   
-                }}
-              >
-                <h3 style={{color:"#1C1F35"}}>Sea Transport Services</h3>
-                <p style={{color:"#666C89"}}>
-                  Following the quality of our service thus having gained trust
-                  of our many clients.
-                </p>
-              </Box>
-            </Box>
-
-            <Box
-              sx={{
-                width: { xs: "100%", md: "45%" },
-                display: "flex",
-                gap: "20px",
-              }}
-            >
-              <Box>
-                <img src={ship} alt="Ship" />
-              </Box>
-              <Box
-                sx={{
-                  borderLeft: "1px solid  #D8D8D8",
-                  paddingLeft: "10px",
-                   
-                }}
-              >
-                <h3 style={{color:"#1C1F35"}}>Sea Transport Services</h3>
-                <p style={{color:"#666C89"}}>
-                  
-                  the quality of our service thus having gained trust of our
-                  many clients.
-                </p>
-              </Box>
-            </Box>
-          </Stack>
+              {servisler.map((servis) => {
+                return (
+                  <>
+                    <Stack sx={{
+                      flexDirection:{xs:"column", md:"row"},
+                      width:{xs:"100%", md:"45%"},
+                      // alignItems:"center",
+                      // justifyContent:"center",
+                      flexWrap:"nowrap",
+                    }}>
+                      <Box
+                        sx={{
+                          padding:"0px 20px",
+                          // backgroundColor: "blue",
+                          
+                        }}
+                      >
+                        <img src={servis.icon} alt="" />
+                      </Box>
+                      <Stack
+                        sx={{
+                          borderLeft: {xs:"none", md:"1px solid #D8D8D8"},
+                          width: { xs: "96%", md: "100%" },
+                          padding:"0px 20px",
+                          // margin:"20px"
+                        }}
+                      >
+                        <h2>{servis.title}</h2>
+                        <p style={{ color: "#666C89", width: "100%" }}>
+                          {servis.subtitle}
+                        </p>
+                      </Stack>
+                    </Stack>
+                  </>
+                );
+              })}
+            </Stack>
+          </Box>
         </Stack>
       </Container>
     </PageContainer>
