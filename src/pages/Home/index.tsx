d62d756e6024cf8b5b57bfb94fe2e925a1ae3576
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-08-23 22:24:40 +0400

    fix small issues

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,8 +17,8 @@ const HomeBanner = () => {
           <Typography component="p" sx={(t) => ({ backgroundColor: t.palette.secondary.light, borderLeft: `4px solid ${t.palette.primary.main}`, fontSize: 14, p: "3px", px: 2, width: 'fit-content' })}>
             Logistics & Supply Chain Solutions
           </Typography>
-          <Typography variant="h2" component="h2" >Your Gateway to any Destination in the World</Typography>
-          <Typography>In augue ligula, feugiat ut nulla consequat. Ut est lacus, molestie in arcu no, iaculis vehicula ipsum. Nunc faucibus, nisl id dapibus finibus, enim diam interdum nulla, sed laoreet risus lectus. </Typography>
+          <Typography my={2} variant="h2" fontWeight={700} component="h2" >Your Gateway to any Destination in the World</Typography>
+          <Typography mb={3}>In augue ligula, feugiat ut nulla consequat. Ut est lacus, molestie in arcu no, iaculis vehicula ipsum. Nunc faucibus, nisl id dapibus finibus, enim diam interdum nulla, sed laoreet risus lectus. </Typography>
           <Button isPrimary title="Explore More" />
         </Container>
       </Stack>
