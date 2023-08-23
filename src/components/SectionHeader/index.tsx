5e2cba1b72a621accf840e1cc43d17de83f72408
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-08-23 21:40:00 +0400

    fix issue on section header

--- src/components/SectionHeader/index.tsx
+++ src/components/SectionHeader/index.tsx
@@ -14,7 +14,7 @@ const SectionHeader = ({ heading, subheading, isCentered = true }: IProps) => {
                 <Typography component="p" bgcolor="#E8E8E8" sx={(t) => ({ borderLeft: `4px solid ${t.palette.primary.main}`, fontSize: 14, p: "3px", px: 2, width: 'fit-content', mx: isCentered ? 'auto' : '0' })}>
                     {heading}
                 </Typography>
-                <Typography my={3} textAlign="center" variant="h4" component="h4" sx={{ fontWeight: 700, textTransform: 'capitalize' }}>
+                <Typography my={3} textAlign={isCentered ? "center" : 'left'} variant="h4" component="h4" sx={{ fontWeight: 700, textTransform: 'capitalize' }}>
                     {subheading}
                 </Typography>
             </div >
