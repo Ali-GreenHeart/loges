b2dfc8b1a8247903ba9f3aa25353513e3ee12595
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-08-25 21:25:35 +0400

    add contact page by Elshad

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,18 +1,18 @@
-import Header from "../../components/Header";
-import Banner from "../../components/Banner";
-import Contact from "../../components/contactUs";
-import Footer from "../../components/Footer";
-import PageContainer from "../../components/PageContainer";
 import Truck from "../../assets/banner/Photo.svg";
+import PageContainer from "../../components/PageContainer";
+import Contact from "../../components/contactUs";
 const ContactPage = () => {
-    return (
-      <>
-       <PageContainer bannerImage={Truck}>
-        <Contact/>
+  return (
+    <>
+      <PageContainer
+        bannerSubtitle="Contact"
+        bannerTitle="Contact Us"
+        bannerImage={Truck}>
+        <Contact />
       </PageContainer>
-        
-      </>
-    );
-  };
-  
-  export default ContactPage;
+
+    </>
+  );
+};
+
+export default ContactPage;
