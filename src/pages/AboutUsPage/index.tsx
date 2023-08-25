c13cbfb331e405d002878890a422a27bf539c1b9
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-08-25 22:05:42 +0400

    add Aboutus banner

--- src/pages/AboutUsPage/index.tsx
+++ src/pages/AboutUsPage/index.tsx
@@ -1,20 +1,20 @@
 import { Stack } from "@mui/material";
-import Truck from "../../assets/banner/Photo.svg";
+import AboutUsBanner from "../../assets/banner/aboutus.png";
 import AboutUs from "../../components/AboutUs";
-import PageContainer from "../../components/PageContainer";
-import WhatWeDo from "../../components/WWD";
-import TeamMemberSection from "../../components/TeamMembersSection";
-import Slider from "../../components/Slider";
-import PricingCard from "../../components/PricingCard";
 import FAQ from "../../components/FAQ";
-import Sponsors from "../Team-Page/Sponsors";
+import PageContainer from "../../components/PageContainer";
+import PricingCard from "../../components/PricingCard";
+import Slider from "../../components/Slider";
+import TeamMemberSection from "../../components/TeamMembersSection";
+import WhatWeDo from "../../components/WWD";
+
 const AboutUsPage = () => {
   return (
     <>
       <PageContainer
         bannerSubtitle="About Us"
         bannerTitle="About Our Logistics"
-        bannerImage={Truck}>
+        bannerImage={AboutUsBanner}>
         <Stack gap={10}>
           <AboutUs />
           <WhatWeDo />
